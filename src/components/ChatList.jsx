5e9ac7bd0900e3c79a35fda6cdde3b46e998d4bc
possible <sumitkumar66416@gmail.com>
2024-06-30 19:22:25 +0530

    style changes

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -18,7 +18,7 @@ export default function ChatList({ setIsChatListVisible }) {
 
   const [chats, setChats] = useState([]);
   const [input, setInput] = useState("");
-  const { currentUser, changeChat, user, loading , setChatId} = useUser();
+  const { currentUser, changeChat, user, loading, setChatId } = useUser();
   const chatList = useRef(null);
 
   useEffect(() => {
@@ -127,7 +127,7 @@ export default function ChatList({ setIsChatListVisible }) {
   };
 
   return (
-    <div className="overflow-y-auto" ref={chatList}>
+    <div className="h-full" ref={chatList}>
       <div className="flex justify-between items-center gap-4 p-3">
         <div className="flex bg-slate-950/50 justify-center items-center rounded-md px-2 w-full">
           <img className="size-5" src="./search.png" alt="" />
@@ -146,73 +146,75 @@ export default function ChatList({ setIsChatListVisible }) {
           onClick={() => setAddIcon(!addIcon)}
         />
       </div>
-      {loading
-        ? filteredUserChat.map(() => (
-            <div className="border-b-2 border-white/25 m-2 p-3 max-w-sm w-full mx-auto">
-              <div className="animate-pulse flex space-x-4">
-                <div className="rounded-full bg-slate-500 h-10 w-10"></div>
-                <div className="flex-1 space-y-3 py-1">
-                  <div className="h-2 bg-slate-500 rounded"></div>
-                  <div className="space-y-3">
-                    <div className="grid grid-cols-3 gap-4">
-                      <div className="h-2 bg-slate-500 rounded col-span-2"></div>
-                      <div className="h-2 bg-slate-600 rounded col-span-1"></div>
-                    </div>
+      <div className="h-full overflow-y-auto">
+        {loading
+          ? filteredUserChat.map(() => (
+              <div className="border-b-2 border-white/25 m-2 p-3 max-w-sm w-full mx-auto">
+                <div className="animate-pulse flex space-x-4">
+                  <div className="rounded-full bg-slate-500 h-10 w-10"></div>
+                  <div className="flex-1 space-y-3 py-1">
                     <div className="h-2 bg-slate-500 rounded"></div>
+                    <div className="space-y-3">
+                      <div className="grid grid-cols-3 gap-4">
+                        <div className="h-2 bg-slate-500 rounded col-span-2"></div>
+                        <div className="h-2 bg-slate-600 rounded col-span-1"></div>
+                      </div>
+                      <div className="h-2 bg-slate-500 rounded"></div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))
-        : filteredUserChat.map((chat) => (
-            <div
-              className={`flex justify-between gap-4 items-center p-2 px-4 md:p-2 lg:py-2 lg:px-3  my-2 border-b border-white/25 cursor-pointer ${
-                chat.isSeen ? "bg-transparent" : "bg-indigo-500"
-              }`}
-              key={chat.chatId}
-              onClick={() => handleSelect(chat)}
-            >
-              <div className="flex gap-4 items-center ">
-                <img
-                  className="size-12 rounded-full"
-                  src={
-                    chat.user.blocked.includes(currentUser.id)
-                      ? "./avatar.png"
-                      : chat.user?.avatar || "/avatar.png"
-                  }
-                  alt=""
-                />
-                <div className="gap-2">
-                  <span className="font-semibold">
-                    {chat.user.blocked.includes(currentUser.id)
-                      ? "User"
-                      : chat.user.username}
-                  </span>
-                  <p className="text-sm text-slate-300">{chat.lastMessage}</p>
-                </div>
-              </div>
-              <button
-                className="hover:text-red-500"
-                onClick={(e) => handleRemoveUser(user, chat, e)}
+            ))
+          : filteredUserChat.map((chat) => (
+              <div
+                className={`flex justify-between gap-4 items-center p-2 px-4 md:p-2 lg:py-2 lg:px-3  my-2 border-b border-white/25 cursor-pointer ${
+                  chat.isSeen ? "bg-transparent" : "bg-indigo-500"
+                }`}
+                key={chat.chatId}
+                onClick={() => handleSelect(chat)}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-5"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                <div className="flex gap-4 items-center ">
+                  <img
+                    className="size-12 rounded-full"
+                    src={
+                      chat.user.blocked.includes(currentUser.id)
+                        ? "./avatar.png"
+                        : chat.user?.avatar || "/avatar.png"
+                    }
+                    alt=""
                   />
-                </svg>
-              </button>
-            </div>
-          ))}
-      {addIcon && <AddUser setAddIcon={setAddIcon} />}
+                  <div className="gap-2">
+                    <span className="font-semibold">
+                      {chat.user.blocked.includes(currentUser.id)
+                        ? "User"
+                        : chat.user.username}
+                    </span>
+                    <p className="text-sm text-slate-300">{chat.lastMessage}</p>
+                  </div>
+                </div>
+                <button
+                  className="hover:text-red-500"
+                  onClick={(e) => handleRemoveUser(user, chat, e)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    />
+                  </svg>
+                </button>
+              </div>
+            ))}
+        {addIcon && <AddUser setAddIcon={setAddIcon} />}
+      </div>
     </div>
   );
 }
